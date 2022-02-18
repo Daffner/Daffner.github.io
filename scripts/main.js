fefcb2e3f6ec6239edbaa3fb1e5a2269f604c5bf
Daffner <79272549+Daffner@users.noreply.github.com>
2022-02-18 13:22:59 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 const pageHeading = document.querySelector('h1');
-let str = document.cookie;
+var str = document.cookie;
 alert(document.cookie);
 pageHeading.textContent = "Hello world 2";
 document.cookie = "testCookie=test";
-alert(document.cookie);
+//alert(document.cookie);
