5c77fbdfd4a3cc813f333753778ec6c738da828e
Daffner <79272549+Daffner@users.noreply.github.com>
2022-03-02 12:58:58 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,4 +6,4 @@ var map = document.getElementById("mapOne");
 //let coords = p1.getAttribute("coords");
 //p1.setAttribute("coords",(""+(w/2)+","+(h/2)+","+radius));
 //alert(map.areas[1].href);
-map.areas[0].coords = ""+(w/2)+","+(h/2)+","+radius;
+map.areas[0].setAttribute("coords",(""+(w/2)+","+(h/2)+","+radius));
