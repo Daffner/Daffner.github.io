d35136879ebb66d061b390d8641b20d16d26d98e
Daffner <79272549+Daffner@users.noreply.github.com>
2022-03-02 10:21:55 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,5 @@ var img = document.getElementById("img1");
 let w = img.clientWidth;
 let h = img.clientHeight;
 var p1 = document.getElementById("p1");
+let coords = p1.getAttribute("coords");
+alert(coords);
