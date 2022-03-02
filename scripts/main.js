adf8e2199a1ed5ab6ba1b88df54c84b5ecc8046a
Daffner <79272549+Daffner@users.noreply.github.com>
2022-03-02 12:49:24 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ let strRadius = "20";
 var img = document.getElementById("img1");
 let w = img.clientWidth;
 let h = img.clientHeight;
-var map = document.getElementById("map1");
+var map = document.getElementById("mapOne");
 //let coords = p1.getAttribute("coords");
 //p1.setAttribute("coords",(""+(w/2)+","+(h/2)+","+radius));
 //alert(map.areas[1].href);
