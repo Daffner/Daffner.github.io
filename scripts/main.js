95c9485c238c8eb0d3b9221777c7b1fbc10819c0
Daffner <79272549+Daffner@users.noreply.github.com>
2022-03-02 10:48:45 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,4 @@ let w = img.clientWidth;
 let h = img.clientHeight;
 var p1 = document.getElementById("p1");
 //let coords = p1.getAttribute("coords");
-p1.setAttribute("coords",""+(w/2)+","+(h/2)+","+radius);
+p1.setAttribute("coords",(""+(w/2)+","+(h/2)+","+radius));
