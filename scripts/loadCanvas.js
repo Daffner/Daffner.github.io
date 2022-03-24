3c09f801a903e176cf69ca3f2bbd2e1ac64eebe4
Daffner <79272549+Daffner@users.noreply.github.com>
2022-03-24 12:39:28 -0400

    Update loadCanvas.js

--- scripts/loadCanvas.js
+++ scripts/loadCanvas.js
@@ -1,1 +1,11 @@
-alert("hi");
+var canvas = document.getElementById("canvas");
+
+var c = canvas.getContext("2d");
+var img = new Image();
+img = document.getElementById("source");
+canvas.setAttribute("width",1026);
+canvas.setAttribute("height",1026);
+img.onload = function(){
+	
+	c.drawImage(img,0,0);
+};
