7c573f3a2f58144ae2c64183d820d2b64feef8dc
Daffner <79272549+Daffner@users.noreply.github.com>
2022-03-25 09:15:50 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,20 @@
 let strRadius = "20";
-var img = document.getElementById("img1");
-let w = img.clientWidth;
-let h = img.clientHeight;
-var map = document.getElementById("mapOne");
-//let coords = p1.getAttribute("coords");
-//p1.setAttribute("coords",(""+(w/2)+","+(h/2)+","+radius));
-//alert(map.areas[1].href);
-map.areas[0].setAttribute("coords",(""+(w/2)+","+(h/2)+","+strRadius));
+var can = document.getElementById("canvas");
+let w = can.clientWidth;
+let h = can.clientHeight;
+//var map = document.getElementById("mapOne");
+//map.areas[0].setAttribute("coords",(""+(w/2)+","+(h/2)+","+strRadius));
+var con = can.getContext("2d");
+let data = con.getImageData(0,0,can.width,can.height);
+let red = [];
+let green = [];
+let blue = [];
+let alpha = [];
+for (int i = 0; i < data.length; i+=4){
+  red[i/4] = data[i];
+  green[i/4] = data[i+1];
+  blue[i/4] = data[i+2];
+  alpha[i/4] = data[i+3];
+  
+}
+
