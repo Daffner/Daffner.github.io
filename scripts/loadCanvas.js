817178e690db63259531a07fe380516ffddddc8f
Daffner <79272549+Daffner@users.noreply.github.com>
2022-03-30 13:47:30 -0400

    Update loadCanvas.js

--- scripts/loadCanvas.js
+++ scripts/loadCanvas.js
@@ -3,8 +3,8 @@ var canvas = document.getElementById("canvas");
 var c = canvas.getContext("2d");
 var img = new Image();
 img = document.getElementById("img1");
-canvas.setAttribute("width",screen.availWidth);
-canvas.setAttribute("height",screen.availHeight);
+canvas.setAttribute("width",screen.availWidth/2);
+canvas.setAttribute("height",screen.availHeight/2);
 img.onload = function(){
 	
 	c.drawImage(img,0,0);
