4c03b0f67514a00e6821555d2a42c0552f4c7125
johnyojohn <johanjohnsjo@gmail.com>
2022-04-08 13:20:26 -0400

    huge initial push to google maps

--- scripts/main.js
+++ scripts/main.js
@@ -6,27 +6,16 @@ let h = can.clientHeight;
 //map.areas[0].setAttribute("coords",(""+(w/2)+","+(h/2)+","+strRadius));
 var con = can.getContext("2d");
 let imgData = con.getImageData(0,0,can.width,can.height);
-var data = imgData.data;
-let red = 0;
-let green = 0;
-let blue = 0;
-//2d array
-var coords = []
-let count = 0;
+let data = imgData.data;
+let red = [];
+let green = [];
+let blue = [];
+let alpha = [];
 for (let i = 0; i < data.length; i+=4){
-  red = data[i];
-  green = data[i+1];
-  blue = data[i+2];
-  //console.log(red + ", " + green + ", "+ blue + "\n");
-  if (red >= 240 && green >= 240 && blue >= 240) {
-    //this is a clickable area
-    coords.push([(i/4)%can.width,(i/4)/can.width]); 
-    count++;
-    //if this works we should have the issue of too many buttons on a point
-  }
+  red[i/4] = data[i];
+  green[i/4] = data[i+1];
+  blue[i/4] = data[i+2];
+  alpha[i/4] = data[i+3];
+  
 }
 
-for (let j = 0; j < coords.length; j++) {
-  let but = document.createElement("BUTTON");
-  but.setAttribute("style","z-index:2; position:absolute; top:"+coords[j][0]+"; left:"+coords[j][1]);
-}
